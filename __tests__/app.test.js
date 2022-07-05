3a235ee174b8f3c0d87e9730397d7b90504810c8
Andrea Cleland <andreaclelan@gmail.com>
2022-07-05 14:03:39 -0600

    GET /secrets when authenticated returns status 200, test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,12 +2,27 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const User = require('../lib/models/User');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   firstName: 'Richard',
   lastName: 'Warmonger',
   email: 'warmonger@defense.gov',
   password: '123456',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  console.log('user', { user });
+  return [agent, user];
 };
 
 describe('auth routes for top secret information', () => {
@@ -38,6 +53,13 @@ describe('auth routes for top secret information', () => {
     const res = await request(app).get('/api/v1/secrets');
     expect(res.status).toEqual(401);
   });
+
+  it('/secrets should return status 200 if authenticated', async () => {
+    const [agent] = await registerAndLogin();
+    const res = await agent.get('/api/v1/secrets');
+    expect(res.status).toEqual(200);
+  });
+
   afterAll(() => {
     pool.end();
   });
